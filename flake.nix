fbddea4e03dfd38412b25bfdbdf5c8c341620640
ReplayCoding <replaycoding@gmail.com>
2021-10-19 12:15:14 -0700

    format flake.nix

--- flake.nix
+++ flake.nix
@@ -9,11 +9,14 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
           packages = with pkgs; [
-            gnumake fennel fnlfmt
+            gnumake
+            fennel
+            fnlfmt
           ];
         };
-    });
+      });
 }
